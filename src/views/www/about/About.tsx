ceb67c21a6a15fcca6a4f6573a4cdcd56e15219a
Makara Nuol <nuolmakara@gmail.com>
2024-05-19 12:29:39 +0700

    add lazy load with suspend in about page

--- src/views/www/about/About.tsx
+++ src/views/www/about/About.tsx
@@ -1,43 +1,24 @@
-import { Button } from "@/components/shared";
-import { Card } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { useAuthContext } from "@/context/useAuthContext";
-import { useState } from "react";
-import { Counter } from "@/features/counter/Counter";
+import { Suspense, lazy } from "react";
+
+const Todo = lazy(() => delayForDemo(import("./TodoComponent")));
+
+const Loading = () => {
+  return <div>Loading...</div>;
+};
+
+async function delayForDemo(promise: Promise<any>) {
+  await new Promise((resolve) => {
+    setTimeout(resolve, 3000);
+  });
+  return await promise;
+}
 
 export default function About() {
-  const { token, saveToken, removeToken } = useAuthContext();
-  const [input, setInput] = useState("");
-
-  const handleOnAddToken = () => {
-    saveToken(input);
-    setInput("");
-  };
-
   return (
     <div className="container mx-auto">
-      <h1 className="text-2xl font-semibold">About</h1>
-      <p className="mt-4">
-        Welcome to the about page! This is a protected route that only logged in
-        users can access.
-      </p>
-
-      <Card className="p-4 mt-2">
-        <p>Token: {token}</p>
-
-        <Input
-          onChange={(e) => setInput(e.target.value)}
-          placeholder="Token"
-          value={input}
-        />
-
-        <div className="flex gap-2 mt-2">
-          <Button onClick={handleOnAddToken}>Add Token</Button>
-          <Button onClick={removeToken}>Remove Token</Button>
-        </div>
-      </Card>
-
-      <Counter />
+      <Suspense fallback={<Loading />}>
+        <Todo />
+      </Suspense>
     </div>
   );
 }
---
+++ src/views/www/about/TodoComponent.tsx
@@ -0,0 +1,10 @@
+const TodoComponent = () => {
+    return (
+        <div>
+        <h1>Todo</h1>
+        <p>Todo</p>
+        </div>
+    );
+}
+
+export default TodoComponent;
